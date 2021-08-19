70f515a18958a130f6617cf31e514445dcf49a01
tianlinyang <tianlinyang303@gmail.com>
2021-08-19 16:16:56 -0400

    areSimilar solution added.

--- src/test.java
+++ src/test.java
@@ -4,6 +4,52 @@ import java.util.*;
 public class test {
 
     /*
+Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+
+Given two arrays a and b, check whether they are similar.
+
+Example
+
+For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+areSimilar(a, b) = true.
+
+The arrays are equal, no need to swap any elements.
+
+For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+areSimilar(a, b) = true.
+
+We can obtain b from a by swapping 2 and 1 in b.
+     */
+
+    boolean areSimilar(int[] a, int[] b) {
+        ArrayList<Integer> difference = new ArrayList<>();
+        //Find differences
+        for (int i = 0; i < a.length; i++) {
+            if ( a[i] != b[i] ) {
+                difference.add(i);
+            }
+        }
+        //Case 1 : ==
+        if (difference.size() == 0) {
+            return true;
+        }
+        //Case 2 : not 2 positions(swap impossible!)
+        if (difference.size() != 2) {
+            return false;
+        }
+        //Case 3 : 2 positions:
+        int id1 = difference.get(0);
+        int id2 = difference.get(1);
+        //If differences could be swap in same value:
+        if (a[id1] == b[id2] && a[id2] == b[id1]) {
+            return true;
+        }
+        //Case 4 : 1,3,4,5...... invalid positions, or 2 positions with differ values
+        return false;
+    }
+
+
+    /*
     Given a rectangular matrix of characters, add a border of asterisks(*) to it.
 
 Example
