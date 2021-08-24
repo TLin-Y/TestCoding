fce773053f28f0adc1fe0410485e42e83094887a
tianlinyang <tianlinyang303@gmail.com>
2021-08-24 14:03:25 -0400

    variableName  added solution

--- src/test.java
+++ src/test.java
@@ -29,6 +29,30 @@ public class test {
         System.out.println(ne.isIPv4Address(".254.255.0"));
     }
 /*
+Correct variable names consist only of English letters, digits and underscores and they can't start with a digit.
+Check if the given string is a correct variable name.
+Example
+For name = "var_1__Int", the output should be
+variableName(name) = true;
+For name = "qq-q", the output should be
+variableName(name) = false;
+For name = "2w2", the output should be
+variableName(name) = false.
+Input/Output
+[execution time limit] 3 seconds (java)
+[input] string name
+Guaranteed constraints:
+1 ≤ name.length ≤ 10.
+[output] boolean
+true if name is a correct variable name, false otherwise.
+ */
+boolean variableName(String name) {
+    return name.matches("^[a-zA-Z_]+[a-zA-Z0-9_]*$");
+}
+
+
+
+/*
 Check if all digits of the given integer are even.
 Example
 For n = 248622, the output should be
